0c9365f983edefc5142e46aa56cd5af1a65a5d8a
Anas Abid <abid.anas3@gmail.com>
2021-11-14 12:23:09 -0800

    chp 21-30

---
+++ app.js
@@ -0,0 +1,106 @@
+// alert("hello class...")
+
+                // Strings
+
+//1)
+// var firstName = prompt("ENTER YOUR FIRST NAME?")
+// var lastName = prompt("ENTER YOUR LAST NAME")
+// // document.write(" WELCOME TO OUR PAGE "+ firstName + lastName )
+// var fullName = firstName + lastName
+// console.log("WELCOME " + fullName)
+
+//2)
+// var phoneName = +prompt("enter ur favourite mobile phone")
+// document.write("My Favourite Phone is : " + phoneName)
+
+
+//3)
+// var string = "Pakistan"
+// var indexNum = string.indexOf("n")
+// document.write("String : " + string)
+// document.write("<br> Index Number : " + indexNum)
+
+//4)
+// var letter = "HELLO WORLD"
+// var indexNum = letter.lastIndexOf("L")
+// console.log("Index Number : " + indexNum)
+
+//5)
+// var word = "PAKISTANI"
+// var indexNum = word.charAt(3)
+// document.write("string : " + word)
+// document.write("<br>character at index 3 : " + indexNum)
+
+//6)
+
+
+//7)
+// var cityName = "hyderabad"
+// var cityName = cityName.replace("hyder","Islam")
+// console.log(cityName)
+
+//8)
+// var message = "Ali and Sami are best friends. They play cricket and football together."
+// var message = message.replace(/and/g,"&")
+// console.log(message)
+
+//9)
+// var A = ["c 
+
+
+//10)
+// var text = "peanuts";
+// var uppercase = text.toUpperCase();
+// document.write("User Input = "+text)
+// document.write("<br>Upper Case = "+uppercase)
+
+// 11)
+// var text = "javascript";
+// console.log(typeof text)
+// let output = text.replace((t)=> {
+
+//     console.log(t)
+//     t.charAt(0).toUpperCase();
+//     return t
+// })
+// document.write("User Input = "+text)
+// console.log("<br>Title Case = "+output)
+
+
+//12)
+// var num = 35.36 
+// var newNum =Math.trunc()
+// document.write(newNum)
+
+
+//14)
+// var itemsName = ["cake", "apple pie", "cookie", "chips", "patties"]
+
+
+
+
+
+
+// //15)
+// var inputPass = +prompt("PLEASE ENTER YOUR PASSWORD")
+
+
+
+//16)
+// var university = "University of Karachi";
+// for (var i = 0; i < university.length; i++) {
+//     document.write(university.charAt(i));
+// }
+
+
+//17)
+// var userInput = "Pakistan"
+// var lastChar = userInput.charAt(7)
+// console.log(lastChar)
+
+//18)
+
+
+
+
+
---
+++ chp 26-30/app.js
@@ -0,0 +1,68 @@
+// alert("HELLO CLASS....!")
+
+                                        // MATH METHODS
+
+
+
+//1)
+// var integer = +prompt("PLEASE ENTER AN INTEGER !")
+// document.write("NUMBER : " + integer)
+// var round =Math.round(integer)
+// document.write("<br>ROUND OF VALUE : " + round)
+// var floor =Math.floor(integer)
+// document.write("<br>FLOOR VALUE : " + floor)
+// var ceil =Math.ceil(integer)
+// document.write("<br>CEIL VALUE : " + ceil)
+
+
+//2)
+// var integer = +prompt("PLEASE ENTER A NEGATIVE INTEGER !")
+// document.write("NUMBER : " + integer)
+// var round =Math.round(integer)
+// document.write("<br>ROUND OF VALUE : " + round)
+// var floor =Math.floor(integer)
+// document.write("<br>FLOOR VALUE : " + floor)
+// var ceil =Math.ceil(integer)
+// document.write("<br>CEIL VALUE : " + ceil)
+
+
+//3)
+// console.log(Math.abs(-4));
+// console.log(Math.abs(5));
+
+//4)
+// var randomNum = Math.trunc((Math.random()*6)+1)
+// document.write("RANDOM NUMBER : "+ randomNum)
+
+//5)
+// var randomNum = Math.trunc((Math.random()*2)+1)
+// document.write("Number = " + randomNum)
+// if(randomNum==1){
+//     document.write("<br>Random Coin Value = Heads")
+// }
+// else if(randomNum==2){
+//     document.write("<br>Random Coin Value = Tails")
+// }
+
+//6)
+// var randomNum = Math.floor(Math.random() * 101);
+// document.write(randomNum)
+
+//7)
+// var userInput = prompt("PLEASE ENTER YOUR WEIGHT IN KILOGRAMS")
+// document.write("THE WEIGHT OF USER IS " + userInput)
+
+//8)
+// var secretNum = 9
+// var enterNum = +prompt("PLEASE ENTER A NUM FROM 1 to 10...!")
+// if(enterNum==secretNum){
+//     alert("CONGRATULATIONS!!!")
+
+// }
+// else{
+//     alert("TRY AGAIN")
+// }
+
+
+
+                                    /////////////////////////////////////////////////////////////////////////////////
